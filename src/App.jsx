4402c06d35d71a9798cd1a565a7a5f8710d5732a
Bogale Demas <bogidemas@gmail.com>
2025-03-03 11:15:35 +0300

    project stracture

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+function App() {
+  return (
+    <div className="w-full p-10 container mx-auto lg:mt-4">
+      <div className="">
+        <header>Header</header>
+        <main>Main</main>
+        <div className="">Cards</div>
+
+        <div className="text-center text-slate-500 py-8">
+          Challenge by
+          <a
+            className="px-2 font-semibold text-blue-400"
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a className="px-2 font-semibold text-blue-400" href="#">
+            Bogale Demas
+          </a>
+          .
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
